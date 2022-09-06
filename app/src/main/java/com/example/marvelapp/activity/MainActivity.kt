04e51d0542440c5321fd07834d75e1a72df858a7
AndresConfortiGL <andres.conforti@globant.com>
2022-09-05 22:21:39 -0300

    [O
    First Commit
    
    added testing
    
    fixed code mistakes
    
    fixed comments
    
    fixed comments
    
    colors fixed

---
+++ app/src/main/java/com/example/marvelapp/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.marvelapp.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marvelapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        fun newInstance(context: Context): Intent = Intent(context, MainActivity::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/marvelapp/activity/SplashScreenActivity.kt
@@ -0,0 +1,43 @@
+package com.example.marvelapp.activity
+
+import android.animation.Animator
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marvelapp.databinding.SplashScreenBinding
+import com.example.marvelapp.mvvm.viewmodel.SplashScreenViewModel
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: SplashScreenBinding
+    private val viewModel: SplashScreenViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel.splashScreenState.observe({ lifecycle }, ::changingState)
+        viewModel.startAnimation()
+    }
+
+    private fun changingState(SplashScreenData: SplashScreenViewModel.SplashScreenData) {
+        when (SplashScreenData.state) {
+            SplashScreenViewModel.SplashScreenState.START -> setListener()
+            SplashScreenViewModel.SplashScreenState.DONE -> launchMainActivity()
+        }
+    }
+
+    private fun launchMainActivity() {
+        startActivity(MainActivity.newInstance(this))
+        finish()
+    }
+
+    private fun setListener() {
+        val animatorListener = object : Animator.AnimatorListener {
+            override fun onAnimationEnd(animation: Animator?) = viewModel.splashDone()
+            override fun onAnimationStart(animation: Animator?) {}
+            override fun onAnimationCancel(animation: Animator?) {}
+            override fun onAnimationRepeat(animation: Animator?) {}
+        }
+        binding.lottieSplashScreen.addAnimatorListener(animatorListener)
+    }
+}
