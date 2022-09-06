aa16e76c78d953da1efb9abdc800a927466915de
AndresConfortiGL <andres.conforti@globant.com>
2022-09-05 22:21:39 -0300

    [O
    First Commit
    
    added testing
    
    fixed code mistakes
    
    fixed comments
    
    fixed comments
    
    colors fixed

---
+++ app/src/main/java/com/example/marvelapp/mvvm/viewmodel/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.marvelapp.mvvm.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/example/marvelapp/mvvm/viewmodel/SplashScreenViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.marvelapp.mvvm.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SplashScreenViewModel : ViewModel() {
+    private val mutableLiveData: MutableLiveData<SplashScreenData> = MutableLiveData()
+    val splashScreenState: LiveData<SplashScreenData> get() = mutableLiveData
+
+    fun splashDone() {
+        mutableLiveData.value = SplashScreenData(SplashScreenState.DONE)
+    }
+
+    fun startAnimation() {
+        mutableLiveData.value = SplashScreenData(SplashScreenState.START)
+    }
+
+    data class SplashScreenData(
+        val state: SplashScreenState
+    )
+
+    enum class SplashScreenState {
+        START,
+        DONE,
+    }
+}
